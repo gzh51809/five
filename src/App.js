f0ff75c613b7ea3033b019583f29a963e12037b8
theaGodzilla <464843022@qq.com>
2019-01-31 13:47:43 +0800

    脚手架

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './public/App.scss';
+
+import { Menu, Icon } from 'antd';
+import MenuItem from 'antd/lib/menu/MenuItem';
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      menu:[
+        {
+          text:'首页',
+          path:'/home',
+          name:'Home',
+          icon:'home'
+        },{
+          text:'商城',
+          path:'/shop',
+          name:'Shop',
+          icon:'shopping'
+        },{
+          text:'海外服务',
+          path:'/server',
+          name:'Server',
+          icon:'dribbble-square'
+        },{
+          text:'到店',
+          path:'/store',
+          name:'Store',
+          icon:'security-scan'
+        },{
+          text:'我的',
+          path:'/person',
+          name:'Person',
+          icon:'user'
+        }
+      ],
+      current:'/home'
+    }
+    this.handleclick=this.handleclick.bind(this);
+  }
+  handleclick({ item, key, keyPath }){
+    this.setState({
+      current:key
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <Menu
+        mode="horizontal"
+        onClick={this.handleclick}
+        >
+          {
+            this.state.menu.map(item=>{
+              return (
+                <MenuItem key={item.path}>
+                  <Icon type={item.icon} />{item.text}
+                </MenuItem>
+              )
+            })
+          }
+        </Menu> */}
+        
       </div>
     );
   }
